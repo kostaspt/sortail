353589dc919f37d0e08b7aab90c96d402b8dca24
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2022-07-21 20:30:59 +0300

    cleanup

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Sortail</title>
+        <meta name="description" content="Sort Tailwind CSS classes on the fly" />
+        <link rel="icon" href="/public/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Sortail</h1>
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
